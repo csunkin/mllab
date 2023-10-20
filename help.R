69bcb2639b094fd4f955f0316f0764acd14c4039
Furtado Jr., O <ovande.furtado@csun.edu>
2023-10-19 18:15:14 -0700

    theme test

--- help.R
+++ help.R
@@ -34,3 +34,18 @@ hugo mod get -u ./... # latest developments
 
 # only showing related under people
 ## the solution is to ensure all items have `authors` = <folder-name>
+
+# temp
+module github.com/wowchemy/starter-research-group
+
+go 1.15
+
+require (
+  github.com/wowchemy/wowchemy-hugo-modules/wowchemy v0.0.0-20210209220000-aa4fe0c75726 // indirect
+  github.com/wowchemy/wowchemy-hugo-modules/wowchemy-cms v0.0.0-20210209220000-aa4fe0c75726 // indirect
+  github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-core v0.1.0 // indirect
+  github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify v1.0.0 // indirect
+  github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms v1.0.0 // indirect
+  github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-seo v0.1.0 // indirect
+  github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy/v5 v5.7.0 // indirect
+)
